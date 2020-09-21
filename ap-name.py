c85ce73a429596146d315a5f7c6de98c9face7af
jkelley0021 <jeffinarizona@gmail.com>
2020-09-20 21:15:59 -0700

    Update ap-name.py

--- ap-name.py
+++ ap-name.py
@@ -6,7 +6,7 @@ import re
 directory = '/var/lib/awx/wlc'
 
 for filename in os.listdir(directory):
-    if filename.startswith('input'):
+    if filename.startswith('in'):
         f = open(filename)
         # open a new file in append mode
         with open ('output.txt', 'a') as file:   
