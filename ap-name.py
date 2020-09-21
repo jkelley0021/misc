33e19aea2e5b60cdbf567a94b34e043f2cb3f0ca
jkelley0021 <jeffinarizona@gmail.com>
2020-09-20 20:55:29 -0700

    Add files via upload

---
+++ ap-name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# the OS module allows you to interface with the underlying operating system
+import os
+# re — regular expression operations
+import re
+
+directory = '/var/lib/awx/wlc'
+
+for filename in os.listdir(directory):
+    if filename.startswith('input'):
+        f = open(filename)
+        # open a new file in append mode
+        with open ('output.txt', 'a') as file:   
+            for line in f:
+                match = re.findall('ap+[a-zA-Z]+[a-zA-Z]+\S+\S', line)
+                if match:
+                    file.writelines(match)
+                    file.write("\n")
+        file.close()
