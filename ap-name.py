286bc5f7a489c3bd527391e7df380f23f89ff1fd
jkelley0021 <jeffinarizona@gmail.com>
2020-09-21 16:21:49 -0700

    Update ap-name.py

--- ap-name.py
+++ ap-name.py
@@ -1,18 +1,12 @@
-# the OS module allows you to interface with the underlying operating system
-import os
 # re — regular expression operations
 import re
 
-directory = '/var/lib/awx/wlc'
-
-for filename in os.listdir(directory):
-    if filename.startswith('in'):
-        f = open(filename)
-        # open a new file in append mode
-        with open ('/var/lib/awx/wlc/output.txt', 'a') as file:   
-            for line in f:
-                match = re.findall('ap+[a-zA-Z]+[a-zA-Z]+\S+\S', line)
-                if match:
-                    file.writelines(match)
-                    file.write("\n")
-        file.close()
+f = open("/var/lib/awx/wlc/input.txt", "r")
+    # open a new file in append mode
+with open ('/var/lib/awx/wlc/output.txt', 'a') as file:   
+    for line in f:
+        match = re.findall('ap+[a-zA-Z]+[a-zA-Z]+\S+\S', line)
+        if match:
+            file.writelines(match)
+            file.write("\n")
+file.close()
