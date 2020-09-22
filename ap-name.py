fc1bdebc3acf14b61f5620ca10ff7ed6a03ce177
jkelley0021 <jeffinarizona@gmail.com>
2020-09-21 19:40:15 -0700

    Update ap-name.py

--- ap-name.py
+++ ap-name.py
@@ -3,7 +3,7 @@ import re
 
 f = open("/var/lib/awx/wlc/input.txt", "r")
     # open a new file in append mode
-with open ('/var/lib/awx/wlc/output.txt', 'a') as file:   
+with open ('/var/lib/awx/wlc/output.csv', 'a') as file:   
     for line in f:
         match = re.findall('ap+[a-zA-Z]+[a-zA-Z]+\S+\S', line)
         if match:
