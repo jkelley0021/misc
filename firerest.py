fd60f074fdebfa46a2209505274ac9f036480a89
jkelley0021 <jeffinarizona@gmail.com>
2020-09-27 12:39:05 -0700

    Update firerest.py

--- firerest.py
+++ firerest.py
@@ -7,10 +7,14 @@ net_obj = {
     'value': '198.18.1.0/24',
 }
 
+response = client.create_object('networks', net_obj)
+
 net_obj = { 
     'name': 'NetObjViaAPI-2',
     'value': '198.18.2.0/24',
 }
+
+response = client.create_object('networks', net_obj)
 
 net_obj = { 
     'name': 'NetObjViaAPI-3',
