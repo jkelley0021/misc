07f51dbfd3752ce29f2fa7a0dd2c75345df89953
jkelley0021 <jeffinarizona@gmail.com>
2020-09-27 13:02:43 -0700

    Update firerest.py

--- firerest.py
+++ firerest.py
@@ -2,6 +2,7 @@ from fireREST import Client
 
 client = Client(hostname='192.168.0.239', username='api-user', password='Firetruck1!')
 
+{
 net_obj = { 
     'name': 'NetObjViaAPI-1',
     'value': '198.18.1.0/24',
@@ -13,5 +14,6 @@ net_obj = {
 net_obj = { 
     'name': 'NetObjViaAPI-3',
     'value': '198.18.3.0/24',
+}
 }
 response = client.create_object('networks', net_obj)
