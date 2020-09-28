ace541fda7ebcab958614229ca210834a5947a5e
jkelley0021 <jeffinarizona@gmail.com>
2020-09-27 18:34:35 -0700

    Add files via upload

---
+++ get-network-object.py
@@ -0,0 +1,8 @@
+from fireREST import Client
+
+client = Client(hostname='192.168.0.239', username='api-user', password='Firetruck1!')
+
+
+obj_name = 'NetObjViaAPI-2'
+obj_id = client.get_object_id_by_name('networks', 'NetObjViaAPI-2')
+obj_payload = client.get_object('networks', obj_id)[0].json()
