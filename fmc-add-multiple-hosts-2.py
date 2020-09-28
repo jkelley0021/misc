25e07568df20b59dab5edf043bf31b4f346c4c5c
jkelley0021 <jeffinarizona@gmail.com>
2020-09-28 15:38:49 -0700

    Add files via upload

---
+++ fmc-add-multiple-hosts-2.py
@@ -0,0 +1,479 @@
+from fireREST import Client
+
+client = Client(hostname='192.168.0.239', username='api-user', password='Firetruck1!')
+
+net_obj = {
+    'name': 'EAZ-MPWEB5V',
+    'value': '172.17.0.160',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'EAZ-MQWEB1V',
+    'value': '172.17.0.30',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'EAZ-MQWEB2V',
+    'value': '172.17.0.40',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'EAZ-MSVC1',
+    'value': '204.153.155.106',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'ECOMTST2',
+    'value': '172.17.0.60',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'ECOMTST4',
+    'value': '172.17.0.70',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'ECOMTST5',
+    'value': '172.17.0.210',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'IWEB01',
+    'value': '172.17.0.110',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'IWEB02',
+    'value': '172.17.0.120',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'IWEB03',
+    'value': '172.17.0.130',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'IWEB04',
+    'value': '172.17.0.140',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'IWEB05',
+    'value': '172.17.0.150',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'IWEB06',
+    'value': '172.17.10.160',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-SJL-A03',
+    'value': '10.1.64.166',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-WEB-SA01',
+    'value': '10.1.10.22',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-WEB-SA02',
+    'value': '10.1.10.26',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MP-CMS-SA01',
+    'value': '10.1.72.98',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MP-WEB-SA01',
+    'value': '10.1.10.25',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MP-WEB-SA02',
+    'value': '10.1.64.30',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MS-PIR-FW01N01',
+    'value': '10.1.66.242',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MS-PIR-FW01N02',
+    'value': '10.1.66.243',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MS-PIR-FW01N03',
+    'value': '10.1.66.244',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MS-WEB-SA01',
+    'value': '10.1.10.21',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-PIN-SA01',
+    'value': '10.1.64.123',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-PIN-SW01',
+    'value': '10.1.64.124',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MP-PIN-SA01',
+    'value': '10.1.64.103',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MP-PIN-SW01',
+    'value': '10.1.64.104',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MT-PIN-SA01',
+    'value': '10.1.64.161',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MT-PIN-SW01',
+    'value': '10.1.64.162',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MDIR04',
+    'value': '10.1.5.134',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MDIR05',
+    'value': '10.1.5.135',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MDIR07',
+    'value': '10.1.5.128',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MDIR08',
+    'value': '10.1.5.125',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR50',
+    'value': '10.1.5.122',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR51',
+    'value': '10.1.5.59',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR52',
+    'value': '10.1.5.60',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR53',
+    'value': '10.1.5.61',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR54',
+    'value': '10.1.5.62',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR55',
+    'value': '10.1.5.63',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR56',
+    'value': '10.1.5.64',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR57',
+    'value': '10.1.5.65',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR58',
+    'value': '10.1.5.66',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR59',
+    'value': '10.1.5.67',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR60',
+    'value': '10.1.5.68',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR61',
+    'value': '10.1.5.69',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR62',
+    'value': '10.1.5.70',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR63',
+    'value': '10.1.5.71',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR64',
+    'value': '10.1.5.72',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR65',
+    'value': '10.1.5.73',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'baz-mpir65',
+    'value': '10.1.10.165',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR66',
+    'value': '10.1.10.166',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'baz-mpir66',
+    'value': '10.1.5.74',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR67',
+    'value': '10.1.10.167',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'baz-mpir67',
+    'value': '10.1.5.94',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MPIR68',
+    'value': '10.1.10.168',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'baz-mpir68',
+    'value': '10.1.5.98',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MQIR04',
+    'value': '10.1.5.169',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'BAZ-MQIR05',
+    'value': '10.1.5.129',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-BPO-SA01',
+    'value': '10.1.72.199',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-BPO-SA02',
+    'value': '10.1.75.12',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-BPO-SA03',
+    'value': '10.1.75.13',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MD-SJL-A05',
+    'value': '10.1.72.241',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MP-BPO-SA01',
+    'value': '10.1.72.174',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MP-BPO-SA02',
+    'value': '10.1.72.111',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MS-BPO-SA01',
+    'value': '10.1.72.198',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MT-BPO-SA01',
+    'value': '10.1.72.173',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MT-BPO-SA02',
+    'value': '10.1.75.14',
+}
+
+response = client.create_object('hosts', net_obj)
+
+net_obj = {
+    'name': 'MT-BPO-SA03',
+    'value': '10.1.75.15',
+}
+
+response = client.create_object('hosts', net_obj)
