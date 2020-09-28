2d72abe4fbcd9fd714082491f65b5e380ac7031c
jkelley0021 <jeffinarizona@gmail.com>
2020-09-27 18:02:55 -0700

    Add files via upload

---
+++ fmc-object-name-to-id.py
@@ -0,0 +1,6 @@
+from fireREST import Client
+
+client = Client(hostname='192.168.0.239', username='api-user', password='Firetruck1!')
+
+name = 'NET_OBJ'
+uuid = client.get_object_id_by_name('networks', name)
