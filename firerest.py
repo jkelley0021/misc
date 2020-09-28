3b1544a3538ed9095863d880b759e280cbc8c8a3
jkelley0021 <jeffinarizona@gmail.com>
2020-09-28 13:40:25 -0700

    Update firerest.py

--- firerest.py
+++ firerest.py
@@ -2,8 +2,6 @@ from fireREST import Client
 
 client = Client(hostname='192.168.0.239', username='api-user', password='Firetruck1!')
 
-response = client.create_object('hosts', net_obj)
-
 net_obj = {
     'name': 'server-4',
     'value': '4.4.4.4',
