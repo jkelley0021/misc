be815d4f240cb5f11d5ee238091b8702de75fe43
jkelley0021 <jeffinarizona@gmail.com>
2020-09-27 18:08:13 -0700

    Update fmc-object-name-to-id.py

--- fmc-object-name-to-id.py
+++ fmc-object-name-to-id.py
@@ -2,5 +2,5 @@ from fireREST import Client
 
 client = Client(hostname='192.168.0.239', username='api-user', password='Firetruck1!')
 
-name = 'NET_OBJ'
+name = 'server-3'
 uuid = client.get_object_id_by_name('networks', name)
